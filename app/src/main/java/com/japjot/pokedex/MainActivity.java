65aa2624ca7e03744df4d98a279f6682d4c6bc91
Angela Dong <isthisangela@Angelas-MacBook-Pro.local>
2018-02-13 23:44:49 -0800

    layout and others

--- app/src/main/java/com/japjot/pokedex/MainActivity.java
+++ app/src/main/java/com/japjot/pokedex/MainActivity.java
@@ -2,12 +2,45 @@ package com.japjot.pokedex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Utils.Pokemon> pokes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            JSONObject jsonObject = new JSONObject(Utils.json);
+            Iterator<String> keys = jsonObject.keys();
+            while (keys.hasNext()) {
+                String name = keys.next();
+                JSONObject thispoke = jsonObject.getJSONObject(name);
+                pokes.add(new Utils.Pokemon(
+                        name,
+                        thispoke.getString("#"),
+                        thispoke.getString("Attack"),
+                        thispoke.getString("Defense"),
+                        thispoke.getString("FlavorText"),
+                        thispoke.getString("HP"),
+                        thispoke.getString("Sp. Atk"),
+                        thispoke.getString("Sp. Def"),
+                        thispoke.getString("Speed"),
+                        thispoke.getString("Total"),
+                        thispoke.getJSONArray("Type")
+                ));
+            }
+        } catch (JSONException e) {
+            Log.e("json exception", e.getMessage());
+        }
     }
 }
