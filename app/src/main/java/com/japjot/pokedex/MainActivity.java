f612dcdbf0aba3fc44faa58942b5cf6a7097bca7
Japjot Singh <japjot@calvisitor-10-105-132-126.calvisitor.1918.berkeley.edu>
2018-02-16 06:01:33 -0800

    updated stylesa' '

--- app/src/main/java/com/japjot/pokedex/MainActivity.java
+++ app/src/main/java/com/japjot/pokedex/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView text = (TextView) findViewById(R.id.textView);
+//        TextView text = (TextView) findViewById(R.id.textView);
 
         try {
 
